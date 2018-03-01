715b19635098ba2ccdf52e78d1103bce2b448572
Merve Ecevit <merveecevit23@gmail.com>
2018-03-02 00:46:07 +0300

    Add function for getting photos from Flickr API

--- FlickrClone/AppDelegate.swift
+++ FlickrClone/AppDelegate.swift
@@ -9,6 +9,10 @@
 import UIKit
 import SnapKit
 
+let baseUrl = "https://api.flickr.com/services/rest/?method="
+let flickrMethod = "flickr.photos.getRecent"
+let apiKey = "44dc856638867f5d04dd0450b9b1f9cf"
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,7 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = TimelineViewController()
+        let navigationController = UINavigationController(rootViewController: TimelineViewController())
+        window?.rootViewController = navigationController
         return true
     }
 }
---
+++ FlickrClone/Controller/TimelineViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TimelineViewController.swift
+//  FlickrClone
+//
+//  Created by Merve Ecevit on 1/03/18.
+//  Copyright © 2018 Merve Ecevit. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireObjectMapper
+
+class TimelineViewController: UITableViewController {
+    
+    var photos: [Photo]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "Flickr"
+        
+        fetchPhotos()
+    }
+    
+    fileprivate func fetchPhotos(){
+        let url = "\(baseUrl)\(flickrMethod)&api_key=\(apiKey)&per_page=20&page=1&format=json&nojsoncallback=1"
+        Alamofire.request(url).responseObject { (response: DataResponse<RecentPhotoResponse>) in
+            
+            let recentPhotoResponse = response.result.value
+            self.photos = recentPhotoResponse?.photos?.photo
+            if self.photos != nil {
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return photos?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+
+        if indexPath.row == 0 {
+            cell.backgroundColor = .red
+        } else {
+            cell.backgroundColor = .blue
+            cell.textLabel?.text = "\(indexPath.section)"
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 1 {
+            return self.view.frame.width
+        }
+        return 40
+    }
+}
+
