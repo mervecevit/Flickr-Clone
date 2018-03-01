ef1350bd0e10f41a2c7f6dd53d14d2a73edb09b6
Merve Ecevit <merveecevit23@gmail.com>
2018-03-01 22:32:50 +0300

    Added TableViewController for root view and basic pods

---
+++ FlickrClone/TimelineViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TimelineViewController.swift
+//  FlickrClone
+//
+//  Created by Merve Ecevit on 1/03/18.
+//  Copyright © 2018 Merve Ecevit. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FlickrClone' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FlickrClone
+  pod 'SnapKit'
+  pod 'Alamofire'
+  pod 'ObjectMapper'
+  pod 'AlamofireObjectMapper'
+
+end
